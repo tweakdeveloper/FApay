15be0bd119ac6be04e643158cbe0278bec62684e
Nicola Clark <tweakdeveloper@gmail.com>
2022-06-14 12:57:30 -0400

    change AirlineSpinner to be a controlled component

--- src/AirlineSpinner.tsx
+++ src/AirlineSpinner.tsx
@@ -1,11 +1,24 @@
-import airlines from "./data.json";
+import React from "react";
 
-function AirlineSpinner() {
+interface AirlineSpinnerProps {
+  airlines: { code: string; name: string }[];
+  onAirlineChanged(code: string): void;
+}
+
+function AirlineSpinner(props: AirlineSpinnerProps) {
+  const [airline, setAirline] = React.useState("MQ");
   return (
-    <select className="p-1 rounded w-full">
-      {airlines.map((airline) => (
-        <option key={airline.code}>
-          {airline.code} &mdash; {airline.name}
+    <select
+      className="p-1 rounded w-full"
+      onChange={(evt) => {
+        setAirline(evt.target.value);
+        props.onAirlineChanged(evt.target.value);
+      }}
+      value={airline}
+    >
+      {props.airlines.map((airline) => (
+        <option key={airline.code} value={airline.code}>
+          {airline.name}
         </option>
       ))}
     </select>
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+import React from "react";
+
 import AirlineSpinner from "./AirlineSpinner";
 
+import airlines from "./data.json";
+
 function App() {
+  const [airline, setAirline] = React.useState("MQ");
   return (
     <div className="flex flex-col space-y-4 w-full lg:w-1/3 lg:mx-auto xl:w-1/4">
       <h1 className="text-3xl text-center">FApay</h1>
-      <AirlineSpinner />
+      <AirlineSpinner
+        airlines={airlines}
+        onAirlineChanged={(code: string) => setAirline(code)}
+      />
     </div>
   );
 }
